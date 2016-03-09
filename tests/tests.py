29f938b31238d47455383640cae8b306dc40db28
Pauline Lallinec <12251066@ucdconnect.ie>
2016-03-09 11:55:40 +0000

    wrote a test and corrected bugs; test passed

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,11 @@
-'''
-Created on 8 Mar 2016
+from src import data_scrapping
 
-@author: Saorla
-'''
+def test_neatify_coord():
+    test_string1 = "{[123.123,][}}}"
+    test_string2 = "123.123,][}}}"
+    test_string3 = "{[123.123"
+    assert(data_scrapping.neatify_coord(test_string1) == 123.123)
+    assert(data_scrapping.neatify_coord(test_string2) == 123.123)
+    assert(data_scrapping.neatify_coord(test_string3) == 123.123)
+
+test_neatify_coord()
